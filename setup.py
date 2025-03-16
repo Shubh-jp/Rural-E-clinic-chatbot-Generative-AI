87b7deef3ef1ce728f8e056be3b4a364f3202866
Shubh-jp <dhakadshubham2581@gmail.com>
2025-03-16 23:44:15 +0530

    folder structure added

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name = 'Generative AI Project',
     version= '0.0.0',
-    author= 'Bappy Ahmed',
-    author_email= 'entbappy73@gmail.com',
+    author= 'Shubh Patel ',
+    author_email= 'dhakadshubham2581@gmail.com',
     packages= find_packages(),
     install_requires = []
 
